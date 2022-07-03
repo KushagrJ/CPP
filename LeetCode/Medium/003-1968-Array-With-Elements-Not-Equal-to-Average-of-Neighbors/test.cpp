da4bb277e953eca94f4cbdf0c0c414d151dac221
KushagrJ <kushagr.j@gmail.com>
2022-07-03 11:51:55 +0530

    Update the Repository

---
+++ LeetCode/Medium/003-1968-Array-With-Elements-Not-Equal-to-Average-of-Neighbors/test.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+
+    std::vector<int> rearrangeArray(std::vector<int>& nums)
+    {
+        std::sort(nums.begin(), nums.end());
+
+        std::vector<int> ans(nums.size());
+
+        int i = 0, j = (nums.size() - 1), k = 0;
+
+        while (i < j)
+        {
+            ans[k++] = nums[i++];
+            ans[k++] = nums[j--];
+        }
+
+        if (i == j)
+            ans[k++] = nums[i];
+
+        return ans;
+    }
+};
